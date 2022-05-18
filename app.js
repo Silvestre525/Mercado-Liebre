a21caf7bbccc0c4d1bf1d28b4adb6e6468e7dc6b
Silvestre525 <silvestrevillar02@gmail.com>
2022-05-18 18:27:51 -0300

    termine el top header

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
 app.listen(3000, () => {
-    console.log("servidor corriendo en puerto 3000");
+    console.log("servidor corriendo en puerto 3080");
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/index.html"));
+    res.sendFile(path.join(__dirname, "/views/home.html"));
 })  //dirname es la direccion de dh-heroes en este caso en nuestra computadora 
