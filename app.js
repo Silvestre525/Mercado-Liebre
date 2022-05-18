f7d57035c9ba62e919b3b41f50b208309dd0a3a7
Silvestre525 <silvestrevillar02@gmail.com>
2022-05-18 17:37:21 -0300

    inicie el proyecto de Mercado Liebre

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require("path");
+
+const app = express();
+
+//Ejemplo utilizando modulos de terceros 
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+    console.log("servidor corriendo en puerto 3000");
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"));
+})  //dirname es la direccion de dh-heroes en este caso en nuestra computadora 
