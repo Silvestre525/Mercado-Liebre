a3f2b8d4df6eba8b3126a2d3742451d678b80291
Silvestre525 <silvestrevillar02@gmail.com>
2022-05-24 00:55:39 -0300

    agregue login y registro

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require("path");
 app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: false }));
 
 
 app.listen(3000, ()=>{
@@ -10,3 +13,17 @@ app.listen(3000, ()=>{
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
+
+app.get('/registro', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/views/registro.html'));
+});
+
+app.get("/login", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
+})
+
+////////
+app.post('/datos',(res,req)=>{
+    res.send(req.body);
+
+})
